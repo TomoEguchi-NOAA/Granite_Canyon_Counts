60bdf971540930bc7e90773fe9539839e41578d6
Tomo Eguchi <Tomo.Eguchi@nmfs.local>
2023-10-16 13:38:37 -0700

    Trying to add hyper parameters for the K parameter

--- Jags_Richards_AllData_V3.R
+++ Jags_Richards_AllData_V3.R
@@ -15,8 +15,10 @@ library(bayesplot)
 
 source("Granite_Canyon_Counts_fcns.R")
 
+Run.date <- Sys.Date()
+#Run.date <- "2023-10-13"
 out.file.name <- paste0("RData/JAGS_Richards_pois_bino_v3_AllYears_",
-                        Sys.Date(), ".rds")
+                        Run.date, ".rds")
 
 # I use the "Laake" model because the number of periods per year
 # can be different.
@@ -187,52 +189,130 @@ LOOIC.n <- compute.LOOIC(loglik.array = jm.out$jm$sims.list$log.lkhd,
                          data.array = data.array,
                          MCMC.params = jm.out$MCMC.params)
 
+# There are some (< 2%) bad ones. I should look at which ones are not fitting well.
+ 
+
+
 # Look at Rhat statistics
 max.Rhat <- lapply(jm.out$jm$Rhat, FUN = max, na.rm = T) %>%
   unlist()
 max.Rhat.big <- max.Rhat[which(max.Rhat > 1.1)]
 
 
+mcmc_dens(jm.out$jm$samples, c("Max.alpha", "Max.beta",
+                               "S1.alpha", "S1.beta",
+                               "S2.alpha", "S2.beta",
+                               "P.alpha", "P.beta"))
 
-Nhat. <- data.frame(median = jm.out$jm$q50$Corrected.Est,
-                      LCL = jm.out$jm$q2.5$Corrected.Est,
-                      UCL = jm.out$jm$q97.5$Corrected.Est,
-                      mean = jm.out$jm$mean$Corrected.Est,
-                      Season =  all.start.year + 1)
+mcmc_dens(jm.out$jm$samples, c("BF.Fixed", "VS.Fixed"))
 
-# Nhat.df %>% 
-#   left_join(Laake.estimates, by = "Season") %>%
-#   mutate(delta_Nhat = median - Nhat) -> all.estimates
-# 
-# ggplot(all.estimates) +
-#   geom_point(aes(x = Season, y = median ),
-#              color = "blue") +
-#   geom_errorbar(aes(x = Season, ymin = LCL.x, ymax = UCL.x),
-#                 color = "blue") +
-#   geom_point(aes(x = Season, y = Nhat ),
-#              color = "green") +
-#   geom_errorbar(aes(x = Season, ymin = LCL.y, ymax = UCL.y),
-#                 color = "green")
-
-# mcmc_dens(jm.out.Laake$jm$samples, c("Max.alpha", "Max.beta", 
-#                                      "S1.alpha", "S1.beta", 
-#                                      "S2.alpha", "S2.beta",
-#                                      "P.alpha", "P.beta"))
 
 P1 <- c("P[1]", "P[2]", "P[3]", "P[4]", "P[5]", "P[6]", "P[7]", "P[8]", "P[9]")
 P2 <- c("P[10]", "P[11]", "P[12]", "P[13]", "P[14]", "P[15]", "P[16]", "P[17]", "P[18]")
-P3 <- c("P[19]", "P[20]", "P[21]", "P[22]", "P[23]")
+P3 <- c("P[19]", "P[20]", "P[21]", "P[22]", "P[23]", "P[24]", "P[25]", "P[26]", "P[27]")
+P4 <- c("P[28]", "P[29]", "P[30]", "P[31]")
 
-K3 <- c("K[19]", "K[20]", "K[21]", "K[22]", "K[23]")
+mcmc_trace(jm.out$jm$samples, P1)
+mcmc_trace(jm.out$jm$samples, P2)
+mcmc_trace(jm.out$jm$samples, P3)
+mcmc_trace(jm.out$jm$samples, P4)
+
+
 K2 <- c("K[10]", "K[11]", "K[12]", "K[13]", "K[14]", "K[15]", "K[16]", "K[17]", "K[18]")
 K1 <- c("K[1]", "K[2]", "K[3]", "K[4]", "K[5]", "K[6]", "K[7]", "K[8]", "K[9]")
+K3 <- c("K[19]", "K[20]", "K[21]", "K[22]", "K[23]", "K[24]", "K[25]", "K[26]", "K[27]")
+K4 <- c("K[28]", "K[29]", "K[30]", "K[31]")
+
+mcmc_trace(jm.out$jm$samples, K1)
+mcmc_trace(jm.out$jm$samples, K2)
+mcmc_trace(jm.out$jm$samples, K3)
+mcmc_trace(jm.out$jm$samples, K4)
 
 S1.1 <- c("S1[1]", "S1[2]", "S1[3]", "S1[4]", "S1[5]", "S1[6]", "S1[7]", "S1[8]", "S1[9]")
-S2.1 <- c("S2[1]", "S2[2]", "S2[3]", "S2[4]", "S2[5]", "S2[6]", "S2[7]", "S2[8]", "S2[9]")
-
 S1.2 <- c("S1[10]", "S1[11]", "S1[12]", "S1[13]", "S1[14]", "S1[15]", "S1[16]", "S1[17]", "S1[18]")
+S1.3 <- c("S1[19]", "S1[20]", "S1[21]", "S1[22]", "S1[23]", "S1[24]", "S1[25]", "S1[26]", "S1[27]")
+S1.4 <- c("S1[28]", "S1[29]", "S1[30]", "S1[31]")
+
+mcmc_trace(jm.out$jm$samples, S1.1)
+mcmc_trace(jm.out$jm$samples, S1.2)
+mcmc_trace(jm.out$jm$samples, S1.3)
+mcmc_trace(jm.out$jm$samples, S1.4)
+
+S2.1 <- c("S2[1]", "S2[2]", "S2[3]", "S2[4]", "S2[5]", "S2[6]", "S2[7]", "S2[8]", "S2[9]")
 S2.2 <- c("S2[10]", "S2[11]", "S2[12]", "S2[13]", "S2[14]", "S2[15]", "S2[16]", "S2[17]", "S2[18]")
+S2.3 <- c("S2[19]", "S2[20]", "S2[21]", "S2[22]", "S2[23]", "S2[24]", "S2[25]", "S2[26]", "S2[27]")
+S2.4 <- c("S2[28]", "S2[29]", "S2[30]", "S2[31]")
 
-S1.3 <- c("S1[19]", "S1[20]", "S1[21]", "S1[22]", "S1[23]")
-S2.3 <- c("S2[19]", "S2[20]", "S2[21]", "S2[22]", "S2[23]")
+mcmc_trace(jm.out$jm$samples, S2.1)
+mcmc_trace(jm.out$jm$samples, S2.2)
+mcmc_trace(jm.out$jm$samples, S2.3)
+mcmc_trace(jm.out$jm$samples, S2.4)
 
+# Look at the abudance estimates:
+Nhat. <- data.frame(median = jm.out$jm$q50$Corrected.Est,
+                    LCL = jm.out$jm$q2.5$Corrected.Est,
+                    UCL = jm.out$jm$q97.5$Corrected.Est,
+                    mean = jm.out$jm$mean$Corrected.Est,
+                    Year =  all.start.year + 1,
+                    Season = paste0(all.start.year, "/", all.start.year+1))
+
+N.hats <- data.frame(Season = rep(Nhat.$Season, each = nrow(jm.out$jm$mean$N)),
+                     Day = rep(1:nrow(jm.out$jm$mean$N), times = length(Nhat.$Season)),
+                     Mean = as.vector(jm.out$jm$mean$N),
+                     LCL = as.vector(jm.out$jm$q2.5$N),
+                     UCL = as.vector(jm.out$jm$q97.5$N))
+
+ggplot(N.hats %>% group_by(Season)) + 
+  geom_ribbon(aes(x = Day, ymin = LCL, ymax = UCL),
+              fill = "blue", alpha = 0.5) +
+  geom_path(aes(x = Day, y = Mean)) + 
+  facet_wrap(~ Season)
+
+# These are not the best estimates because they were not updated as more data
+# were collected. I should use the output from the most recent WinBUGS run for 
+# the last 9 years.
+Reported.estimates <- read.csv(file = "Data/all_estimates_2023.csv") %>%
+  select(-c(X, Year))
+
+WinBugs.9yr.out <- readRDS(file = "RData/WinBUGS_9yr_v2_min85.rds")
+Corrected.Est <- WinBugs.9yr.out$BUGS_out$sims.list$Corrected.Est
+seasons <- c("2006/2007", "2007/2008", "2009/2010", "2010/2011", 
+             "2014/2015", "2015/2016", "2019/2020", "2021/2022",
+             "2022/2023")
+
+Durban.abundance.df <- data.frame(Season = seasons,
+                                  Nhat = apply(Corrected.Est,
+                                               FUN = mean,
+                                               MARGIN = 2),
+                                  # CV = apply(Corrected.Est,
+                                  #            FUN = function(x) 100*sqrt(var(x))/mean(x),
+                                  #            MARGIN = 2),
+                                  # median = apply(Corrected.Est, 
+                                  #                FUN = median, 
+                                  #                MARGIN = 2),
+                                  LCL = apply(Corrected.Est, 
+                                              MARGIN = 2, 
+                                              FUN = quantile, 0.025),
+                                  UCL = apply(Corrected.Est, 
+                                              MARGIN = 2, 
+                                              FUN = quantile, 0.975),
+                                  Method = "Durban")
+
+Reported.estimates %>%
+  filter(Method == "Laake") %>%
+  rbind(Durban.abundance.df) -> previous.estimates
+
+
+Nhat. %>%
+  left_join(previous.estimates, by = "Season") %>%
+  mutate(delta_Nhat = mean - Nhat) -> all.estimates
+# 
+ggplot(all.estimates) +
+  geom_point(aes(x = Season, y = mean ),
+             color = "blue") +
+  geom_errorbar(aes(x = Season, ymin = LCL.x, ymax = UCL.x),
+                color = "blue") +
+  geom_point(aes(x = Season, y = Nhat ),
+             color = "green") +
+  geom_errorbar(aes(x = Season, ymin = LCL.y, ymax = UCL.y),
+                color = "green")
